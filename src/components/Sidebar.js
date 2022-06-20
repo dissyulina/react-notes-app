2be3e5ad068cf03930d005d56073ab62908fff05
Dissy Ulina <dissyulina@gmail.com>
2022-06-20 14:12:04 +0000

    Add delete functionality to notes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,22 @@
 import React from "react"
 
 export default function Sidebar(props) {
+    // If you need to pass extra parameters other than the event to your event function, provide the whole callback function like delete button below.
     const noteElements = props.notes.map((note, index) => (
         <div key={note.id}>
             <div
-                
                 className={`title ${
                     note.id === props.currentNote.id ? "selected-note" : ""
                 }`}
                 onClick={() => props.setCurrentNoteId(note.id)}
             >
                 <h4 className="text-snippet">{note.body.split("\n")[0]}</h4>
+                <button 
+                    className="delete-btn" 
+                    onClick={(event) => props.deleteNote(event, note.id)}
+                >
+                    <i className="gg-trash trash-icon"></i>
+                </button>
             </div>
         </div>
     ))
